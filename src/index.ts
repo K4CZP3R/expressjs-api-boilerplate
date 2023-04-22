0202965729e3a861b9db8bc5cdfe9c265905327c
Kacper <kacper@kacperserewis.net>
2023-04-22 10:03:55 +0200

    use zod for env validation

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,9 @@
-import { getEnvironmentInterface } from "./helpers/dotenv.helper";
 import http from "http";
 import { App } from "./app";
 import { getDebug } from "./helpers/debug.helper";
+import { getEnvironment } from "./helpers/dotenv.helper";
 
-let env = getEnvironmentInterface();
-if (!env.SERVER_PORT) {
-	console.error("port for the server is not specified!");
-	process.exit(1);
-}
+const env = getEnvironment();
 
 const debug = getDebug();
 
@@ -15,5 +11,5 @@ const app = new App();
 const server = http.createServer(app.app);
 
 server.listen(env.SERVER_PORT, "0.0.0.0", () => {
-	debug(`App listening on port ${env.SERVER_PORT}!`);
+  debug(`App listening on port ${env.SERVER_PORT}!`);
 });
